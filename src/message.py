3b0cc19daf7ef902e3f2c93feae22ac8379207ee
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2020-12-21 15:48:09 -0400

    Update README.md

--- src/message.py
+++ src/message.py
@@ -26,3 +26,6 @@ chance de gol: {major.opportunity_goals}
 def send_message(token, chat_id, message):
     bot = Bot(token=token)
     bot.send_message(chat_id=chat_id, text=message, parse_mode="HTML")
+
+
+# send_message('1461390419:AAFDPAuxzMka8Qt833PMUNj_RCr9yNM9IEU', 325105532, "ei")
