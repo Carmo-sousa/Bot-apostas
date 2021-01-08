90fe09d2d68946c35620e830e0c5f4f3b3d8b34c
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2021-01-07 22:05:51 -0400

    style: Adicionado os tipos das variáveis

--- app.py
+++ app.py
@@ -8,87 +8,91 @@ from score_bing.team import Team
 from config import CHAT_ID, TELEGRAM_TOKEN, BASE_API_URL, header, params
 
 # Guarda o id dos jogos que já tiveram seu alerta emitido
-repeated = []
+repeated: list[str] = []
 
 
-def bot():
-    rec = request(BASE_API_URL, header, params)
-    if rec:
-        data = rec
-        for item in data:
-            live = is_live(item)
-            if live:
-                _id = live.get("id")
-                statistic = Statistics(live)
+def bot() -> None:
+    message: str = ""
+    data: list[dict, str] = request(BASE_API_URL, header, params)
 
-                host = Team(statistic.host, statistic.status)
-                guest = Team(statistic.guest, statistic.status)
+    if data:
+        for row in data:
+            row: dict = live(row)
 
-                total_goals = host.goals + guest.goals
+            if not row:
+                continue
 
-                if (
-                    host.apm >= 1.0
-                    and host.opportunity_goals >= 15
-                    and total_goals <= 2
-                    and _id not in repeated
-                ):
-                    message = mount_message(
-                        host,
-                        guest,
-                        "Oportunidades em escanteios",
-                        statistic.league_name,
-                        statistic.status,
-                    )
-                    send_message(TELEGRAM_TOKEN, CHAT_ID, message)
-                    repeated.append(_id)
+            _id: str = row.get("id")
+            statistic: Statistics = Statistics(row)
 
-                elif (
-                    guest.apm >= 1.0
-                    and guest.opportunity_goals >= 15
-                    and total_goals <= 2
-                    and _id not in repeated
-                ):
-                    message = mount_message(
-                        guest,
-                        host,
-                        "Oportunidades em escanteios",
-                        statistic.league_name,
-                        statistic.status,
-                    )
-                    send_message(TELEGRAM_TOKEN, CHAT_ID, message)
-                    repeated.append(_id)
+            host: Team = Team(statistic.host, statistic.status)
+            guest: Team = Team(statistic.guest, statistic.status)
 
-                elif (
-                    host.apm >= 1.3
-                    and host.opportunity_goals > 15
-                    and total_goals <= 2
-                    and _id not in repeated
-                ):
-                    message = mount_message(
-                        host,
-                        guest,
-                        "Oportunidades em gol",
-                        statistic.league_name,
-                        statistic.status,
-                    )
-                    send_message(TELEGRAM_TOKEN, CHAT_ID, message)
-                    repeated.append(_id)
+            total_goals: int = host.goals + guest.goals
 
-                elif (
-                    guest.apm >= 1.0
-                    and guest.opportunity_goals > 15
-                    and total_goals <= 2
-                    and _id not in repeated
-                ):
-                    message = mount_message(
-                        guest,
-                        host,
-                        "Oportunidades em gol",
-                        statistic.league_name,
-                        statistic.status,
-                    )
-                    send_message(TELEGRAM_TOKEN, CHAT_ID, message)
-                    repeated.append(_id)
+            if (
+                host.apm >= 1.0
+                and host.opportunity_goals >= 15
+                and total_goals <= 2
+                and _id not in repeated
+            ):
+                message = mount_message(
+                    host,
+                    guest,
+                    "Oportunidades em escanteios",
+                    statistic.league_name,
+                    statistic.status,
+                )
+                send_message(TELEGRAM_TOKEN, CHAT_ID, message)
+                repeated.append(_id)
+
+            elif (
+                guest.apm >= 1.0
+                and guest.opportunity_goals >= 15
+                and total_goals <= 2
+                and _id not in repeated
+            ):
+                message = mount_message(
+                    guest,
+                    host,
+                    "Oportunidades em escanteios",
+                    statistic.league_name,
+                    statistic.status,
+                )
+                send_message(TELEGRAM_TOKEN, CHAT_ID, message)
+                repeated.append(_id)
+
+            elif (
+                host.apm >= 1.3
+                and host.opportunity_goals > 15
+                and total_goals <= 2
+                and _id not in repeated
+            ):
+                message = mount_message(
+                    host,
+                    guest,
+                    "Oportunidades em gol",
+                    statistic.league_name,
+                    statistic.status,
+                )
+                send_message(TELEGRAM_TOKEN, CHAT_ID, message)
+                repeated.append(_id)
+
+            elif (
+                guest.apm >= 1.0
+                and guest.opportunity_goals > 15
+                and total_goals <= 2
+                and _id not in repeated
+            ):
+                message = mount_message(
+                    guest,
+                    host,
+                    "Oportunidades em gol",
+                    statistic.league_name,
+                    statistic.status,
+                )
+                send_message(TELEGRAM_TOKEN, CHAT_ID, message)
+                repeated.append(_id)
 
 
 if __name__ == "__main__":
