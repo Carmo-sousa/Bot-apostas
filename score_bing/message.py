7a020c151e6970e10f8b11c9b3d5b616265b5282
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2021-01-09 18:09:36 -0400

    refactor: :recycle: Remove functions and add class
    Removing mount_message and sends_message and adding the message class that was left with this responsibility

--- score_bing/message.py
+++ score_bing/message.py
@@ -7,32 +7,6 @@ SOCCER_BALL: str = "\U000026BD"
 CORNER: str = "\U000026F3"
 
 
-def mount_message(major, minor, type_message, league, status):
-
-    message = f"""
-<b>{type_message}: {major.name}</b>
-Liga: {league}
-
-{major.name} {major.goals} x {minor.goals} {minor.name}
-
-{major.danger_attack} ataques perigosos em {status} minútos.
-{major.on_target} chutes a gol {SOCCER_BALL}
-{major.off_target} chutes fora {SOCCER_BALL}
-{major.corners} cantos (escanteios) {CORNER}
-
-Posse de bola {major.possession}% x {minor.possession}%
-
-APM: {major.apm: .2f}
-chance de gol: {major.opportunity_goals}
-"""
-    return message
-
-
-def send_message(token, chat_id, message):
-    bot = Bot(token=token)
-    bot.send_message(chat_id=chat_id, text=message, parse_mode="HTML")
-
-
 class Message:
     def __init__(
         self,
